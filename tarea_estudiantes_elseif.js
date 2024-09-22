449ff2c3b759dda449cd6abae6b781ec9961d0c9
LuxoNever <lucasalvaradocabrera@gmail.com>
2024-09-22 17:10:41 -0400

    Gestión de Personas en una Universidad(elseif)

---
+++ tarea_estudiantes_elseif.js
@@ -0,0 +1,95 @@
+class Persona {
+    constructor(nombre, edad, tipo) {
+        this.nombre = nombre 
+        this.edad = edad
+        this.tipo = tipo      
+    }
+
+
+    presentar(){
+
+        console.log (`Hola soy ${this.nombre} y tengo edad ${this.edad} años`)
+
+    }
+}
+
+class Estudiante extends Persona{
+    constructor(nombre, edad, carrera){
+
+        super(nombre, edad)
+        this.carrera = carrera
+        this.tipo = "Estudiante"
+    }
+
+    infoEstudiantes(){
+
+        console.log (`Soy estudiante de ${this.carrera}`)}
+
+    presentar(){
+        console.log (`Hola soy ${this.nombre} tengo ${this.edad} años `)
+    }    
+
+}
+
+class Docente extends Persona{
+    constructor(nombre, edad, materia){
+
+        super(nombre, edad)
+        this.materia = materia
+        this.tipo = "Docente"
+    }
+
+    infoDocente(){
+
+        console.log (`Soy docente de ${this.materia}`)}
+
+    presentar(){
+        console.log (`Hola soy ${this.nombre} tengo ${this.edad} años `)
+}      
+
+}
+
+
+class Administrativo extends Persona{
+    constructor(nombre, edad, puesto){
+
+        super(nombre, edad)
+        this.puesto = puesto
+        this.tipo = "Administrativo"
+    }
+
+    infoAdministrativo(){
+
+        console.log (`Soy administrativo en el puesto de ${this.puesto}`)}
+    presentar(){
+
+        console.log (`Hola soy ${this.nombre} tengo ${this.edad} años`)
+    }  
+
+}
+
+function mostrarInfoGeneral(Persona) {
+    Persona.presentar()
+
+    if (Persona == estudiante){
+        return   Persona.infoEstudiantes();
+    }
+    else if(Persona == docente){
+        return   Persona.infoDocente();
+    }
+    else if (Persona == administrativo){
+        return  Persona.infoAdministrativo();   
+    }
+     else{
+        return "No se encontro el puesto"
+     }     
+  
+}        
+
+
+const estudiante = new Estudiante('Juan', 20, 'Ingeniería');
+const docente = new Docente('María', 35, 'Matemáticas');
+const administrativo = new Administrativo('Pedro', 40, 'Secretario');
+mostrarInfoGeneral(estudiante); // "Hola, soy Juan, estudiante de Ingeniería."
+mostrarInfoGeneral(docente); // "Hola, soy María, docente de Matemáticas."
+mostrarInfoGeneral(administrativo);
